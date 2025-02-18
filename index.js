599e3b64201b44f15da41470557f026f395a790f
Alyona M <alyona.maliassova@gmail.com>
2025-02-17 20:59:02 -0600

    New user default
    Updating javascript so new users get default 6 and 6 dice and "Refresh me" message

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-randomNumber1 = Math.floor(Math.random() * 6) + 1;
-randomDice = `./images/dice-six-faces-${randomNumber1}.png`;
-document.querySelectorAll("img")[0].setAttribute("src", randomDice);
+function rollDice(){
+    randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    randomDice = `./images/dice-six-faces-${randomNumber1}.png`;
+    document.querySelectorAll("img")[0].setAttribute("src", randomDice);
 
-randomNumber2 = Math.floor(Math.random() * 6) + 1;
-randomDice2 = `./images/dice-six-faces-${randomNumber2}.png`;
-document.querySelectorAll("img")[1].setAttribute("src", randomDice2);
+    randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    randomDice2 = `./images/dice-six-faces-${randomNumber2}.png`;
+    document.querySelectorAll("img")[1].setAttribute("src", randomDice2);
 
-if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").textContent="🚩Player 1 Wins!";
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").textContent="🚩Player 1 Wins!";
+    }
+    else if(randomNumber1 < randomNumber2){
+        document.querySelector("h1").textContent="Player 2 Wins!🚩";
+    }
+    else{
+        document.querySelector("h1").textContent="Draw!";
+    }
 }
-else if(randomNumber1 < randomNumber2){
-    document.querySelector("h1").textContent="Player 2 Wins!🚩";
-}
-else{
-    document.querySelector("h1").textContent="Draw!";
+
+if(!localStorage.getItem("visited")){
+    localStorage.setItem("visited", "true");
+} else{
+    rollDice();
 }
