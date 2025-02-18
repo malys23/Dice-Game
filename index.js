9ba884072387ef80bd47a404d7bbe0baf8c50192
Alyona M <alyona.maliassova@gmail.com>
2025-02-17 20:52:17 -0600

    Completed site
    Implemented winner messages.

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ document.querySelectorAll("img")[0].setAttribute("src", randomDice);
 randomNumber2 = Math.floor(Math.random() * 6) + 1;
 randomDice2 = `./images/dice-six-faces-${randomNumber2}.png`;
 document.querySelectorAll("img")[1].setAttribute("src", randomDice2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").textContent="🚩Player 1 Wins!";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").textContent="Player 2 Wins!🚩";
+}
+else{
+    document.querySelector("h1").textContent="Draw!";
+}
